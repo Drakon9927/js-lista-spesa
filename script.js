4b3adcf8f03e1925cc89da7ef20911ada5f090c4
Antonino <drakon9927@libero.it>
2024-04-24 15:11:32 +0200

    m1 fixes

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Creo un Array
-const listaSpesa = {"uova, spinaci, carne, pesce"};
+const listaSpesa = ["uova, spinaci, carne, pesce"];
 
 // Itinerare con For
 console.log("itero gli elementi usando for");
 for (let i = 0; i < listaSpesa.length; i++) {
     const element = listaSpesa[i];
+    console.log(i, element);
     
 }
