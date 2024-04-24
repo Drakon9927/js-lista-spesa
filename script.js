f920139d1d60ffcad88a6f1bb9d3cee226017552
Antonino <drakon9927@libero.it>
2024-04-24 15:19:10 +0200

    M2 - convertire array in while

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ for (let i = 0; i < listaSpesa.length; i++) {
     console.log(i, element);
     
 }
+
+// convertire in ciclo while
+
+console.log("itero gli elementi usando while");
+
+let i = 0;
+while ( i < listaSpesa.length;) {
+    const element = listaSpesa[i];
+    console.log(i, element);
+
+    i++;
+    
+}
+
+console.log("i esiste ancora dopo while", i);
