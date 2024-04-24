18523ebaefda1670f1e758dc35855700deac8512
Antonino <drakon9927@libero.it>
2024-04-24 15:09:34 +0200

    m1 itinerare

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 // Creo un Array
 const listaSpesa = {"uova, spinaci, carne, pesce"};
+
+// Itinerare con For
+console.log("itero gli elementi usando for");
+for (let i = 0; i < listaSpesa.length; i++) {
+    const element = listaSpesa[i];
+    
+}
