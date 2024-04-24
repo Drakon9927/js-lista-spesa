bd106e06c088c7ca68ffb240bf444515cb9bba0b
Antonino <drakon9927@libero.it>
2024-04-24 15:31:24 +0200

    m1 fixes

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // Creo un Array
-const listaSpesa = ["uova, spinaci, carne, pesce"];
+const listaSpesa = ["uova", "spinaci", "carne", "pesce"];
+
+// Inserire in un li
+
+const listaProdotti = document.querySelector("ul");
 
 // Itinerare con For
 console.log("itero gli elementi usando for");
 for (let i = 0; i < listaSpesa.length; i++) {
     const element = listaSpesa[i];
     console.log(i, element);
+    
     
 }
 
@@ -19,10 +24,9 @@ while ( i < listaSpesa.length) {
     console.log(i, element);
 
     i++;
-    
+
+    listaProdotti.innerHTML += `<li>${element}</li>`;
 }
 
 console.log("i esiste ancora dopo while", i);
-
-// Inserire in un li
 
